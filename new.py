2188473304a9ca34c921664e2afb3a3fd1d1bcb6
Manish <manishcprogram@gmail.com>
2020-03-05 23:34:10 +0530

    Task 1 completed

---
+++ new.py
@@ -0,0 +1,4 @@
+# utility[1][1][1]=max(0.5*(stepcost+finalreward+Y*utility[0][0][0])+0.5*(stepcost+Y*utility[1][0][0]), stepcost+Y*utility[1][1][1])
+	# utility[1][0][0]=stepcost+Y*utility[1][0][1]
+	# utility[1][1][0]=stepcost+Y*utility[1][1][1]
+	# utility[1][0][1]=stepcost+Y*utility[1][0][1]
